a5f60095d2792f06522161232bb436a14795f600
Vagner Sousa <vagner.s.sousa@outlook.com>
2022-11-05 22:12:48 -0300

    Exemplos da aula 1

---
+++ tipoNullUndefined.js
@@ -0,0 +1,17 @@
+let input = null;
+let input2;
+
+console.log(input); //retorna null
+console.log(input2);//retorna undefined
+
+if(input === null){
+   console.log('não há informação') 
+}else{
+    console.log(input)
+}
+
+const soma = 10+10;
+const multiplicacao = 10 *10;
+const operacao = (soma + multiplicacao)+10;
+
+console.log(operacao);
