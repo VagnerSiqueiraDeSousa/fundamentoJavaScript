09f92597030dfa29d93f9345c2427f6f8e56fce0
Vagner Sousa <vagner.s.sousa@outlook.com>
2022-11-15 10:26:14 -0300

    codigos tiposVariaveis e truthy-falsy e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+//tipo de dado
+// booleanos
+
+// converão implicita
+//Number() para converte uma string em numero ou (int)
+//String()
+const numero = 456;
+//const numeroString = "456";
+const numeroString = Number("456")
+
+//console.log(numero === numeroString)
+//console.log(numero + Number(numeroString))
+console.log(numero + numeroString);
--- tiposVariaveis.js
+++ tiposVariaveis.js
@@ -1,7 +1,36 @@
-// var
+// var, pode ser usada solta no codigo podendo gerar erro
+//let, sempre declara antes de ser utilizar o valor pode ser alterando mais a frente e tem que ser declarado antes
+//const, o valor não pode ser alterado a frente "valor fixo" e sempre declarado antes
 
-var altura = 5;
-var comprimento = 7;
-area = altura * comprimento;
+//var altura = 5;
+//var comprimento = 7;
+//area = altura * comprimento;
+//console.log(area);
+//var area;
 
+/*
+let forma = 'retangulo'
+let altura = 5;
+let comprimento = 7;
+let area
+
+if(forma === 'retangulo'){
+    area = altura * comprimento; 
+}else{
+    area = (altura * comprimento) /2;
+}
 console.log(area);
+*/
+
+const forma = "triangulo";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === 'quadrado'){
+    area = altura * comprimento; 
+}else{
+    area = (altura * comprimento) /2;
+}
+
+console.log(area)
