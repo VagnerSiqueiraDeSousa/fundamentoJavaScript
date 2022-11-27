fb9d6af323a5417ae28bea89afd098b83162bf4a
Vagner Sousa <vagner.s.sousa@outlook.com>
2022-11-27 18:54:25 -0300

    operadores e funcoes

---
+++ expressoes-funcao.js
@@ -0,0 +1,21 @@
+//declaração de função
+
+// function minhaFuncao(param){
+//     //bloco de codigo
+// }
+// minhaFuncao("param")
+
+//expressao de função
+
+//const soma = function(num1, num2) {return num1 + num2}
+//console.log(soma(1,1));
+
+// diferença principal ????
+
+console.log(apresentar());
+function apresentar(){
+    return "Olá"
+}
+
+console.log(soma(1,1));
+const soma = function(num1, num2) {return num1 + num2}
--- parametros.js
+++ parametros.js
@@ -74,4 +74,10 @@ function operacaoMatematica(numero1, numero2, numero3) {
     return numero1 + numero2 + numero3
    }
    
-   operacaoMatematica(15, 30, 45) // 90
+  console.log(operacaoMatematica(15, 30, 45)) // 90
+
+
+  function comParametro(param){
+    console.log(param)
+  }
+comParametro();
